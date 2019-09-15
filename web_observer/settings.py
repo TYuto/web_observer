0788465f16f5e80cb01f46e4561ed5acc05883d0
Yuto Tamura <yuto.tamur@gmail.com>
2019-09-16 07:32:45 +0900

    [fix] typo

--- web_observer/settings.py
+++ web_observer/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-　  'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'web_observer.urls'
